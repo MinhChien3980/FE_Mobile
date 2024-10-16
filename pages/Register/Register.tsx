7bfff2cfa3bae5cbcd511a1bbd5da2d22066b653
Phan Ngọc Tài <ngoctaiphan.cv@gmail.com>
2024-10-17 00:02:25 +0700

    Thêm các trường còn thiếu cho register(User sẽ bao gồm name, email, phone, password, address, birthdate)

--- pages/Register/Register.tsx
+++ pages/Register/Register.tsx
@@ -1,52 +1,56 @@
+import { RootStackParamList } from "@/app";
 import UserInfor from "@/components/UserInfor/UserInforForm";
-import { useNavigation } from "@/router/NavigationContext";
-import { Box, Button, Center, Heading, Link } from "native-base";
+import { userRegister } from "@/interface/user";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { Box, Button, Center, Heading, Link, ScrollView } from "native-base";
 
 export default function Register() {
-  const { goBack } = useNavigation();
-  const handleRegister = (data: any) => {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const handleRegister = (data: userRegister) => {
     console.log("Đăng ký: ", data);
   };
   return (
-    <Center mt="50%" w="100%">
-      <Box safeArea p="2" w="90%" maxW="290" py="8">
-        <Heading
-          size="lg"
-          color="coolGray.800"
-          _dark={{
-            color: "warmGray.50",
-          }}
-          fontWeight="semibold"
-        >
-          Xin chào!
-        </Heading>
-        <Heading
-          mt="1"
-          color="coolGray.600"
-          _dark={{
-            color: "warmGray.200",
-          }}
-          fontWeight="medium"
-          size="xs"
-        >
-          Đăng kí tài khoản cùng chúng tôi
-        </Heading>
-        <UserInfor
-          isRegister={true}
-          onSubmit={handleRegister}
-          buttonLabel="Đăng ký"
-        />
-      </Box>
-      <Link
+    <ScrollView>
+      <Center mt="10%" w="100%">
+        <Box safeArea p="2" w="90%" maxW="290" py="8">
+          <Heading
+            size="lg"
+            color="coolGray.800"
+            _dark={{
+              color: "warmGray.50",
+            }}
+            fontWeight="semibold"
+          >
+            Xin chào!
+          </Heading>
+          <Heading
+            mt="1"
+            color="coolGray.600"
+            _dark={{
+              color: "warmGray.200",
+            }}
+            fontWeight="medium"
+            size="xs"
+          >
+            Đăng kí tài khoản cùng chúng tôi
+          </Heading>
+          <UserInfor
+            isRegister={true}
+            onSubmit={handleRegister}
+            buttonLabel="Đăng ký"
+          />
+        </Box>
+        {/* <Link
         _text={{
           color: "indigo.500",
           fontWeight: "medium",
           fontSize: "sm",
         }}
-        onPress={goBack}
+        onPress={navigation.goBack}
       >
         Quay về
-      </Link>
-    </Center>
+      </Link> */}
+      </Center>
+    </ScrollView>
   );
 }
