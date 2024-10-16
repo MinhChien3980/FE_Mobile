b4730e1c209199f2b92d5f42224df24674decc4e
HuynhThiMaiPhuong <21130149@st.hcmuaf.edu.vn>
2024-10-16 08:57:26 +0700

    Mỗi thứ 1 ít 3 trang Phương làm và ex navigation giữa trang productListScreen và trang FavoritesScreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,42 @@
-import React from "react";
-import { NativeBaseProvider, Text, Box } from "native-base";
-import Login from "@/pages/Login/Login";
+// import React from "react";
+// import {NativeBaseProvider, Text, Box} from "native-base";
+// import Login from "@/pages/Login/Login";
+// import Home from "@/pages/Home/Home";
+// import Contact from "@/pages/Contact/Contact";
+//
+// export default function App() {
+//     return (
+//         <NativeBaseProvider>
+//             {/*<Login></Login>*/}
+//             {/*{<Home></Home>}*/}
+//             <Contact></Contact>
+//         </NativeBaseProvider>
+//     );
+// }
+import React from 'react';
 
-export default function App() {
-  return (
-    <NativeBaseProvider>
-      <Login></Login>
-    </NativeBaseProvider>
-  );
-}
+import {createStackNavigator} from '@react-navigation/stack';
+import ProductListScreen from '../pages/WishList/ProductListScreen';
+import FavoritesScreen from '../pages/WishList/FavoritesScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+        <Stack.Navigator initialRouteName="ProductList">
+            <Stack.Screen
+                name="ProductList"
+                component={ProductListScreen}
+                options={{title: 'Danh sách sản phẩm'}}
+            />
+            <Stack.Screen
+                name="Favorites"
+                component={FavoritesScreen}
+                options={{title: 'Danh sách yêu thích'}}
+            />
+        </Stack.Navigator>
+
+    );
+};
+
+export default App;
