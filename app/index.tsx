f9080e44bef8a1dbbf7eb76d94691492c2efe587
HuynhThiMaiPhuong <21130149@st.hcmuaf.edu.vn>
2024-10-16 10:50:07 +0700

    Hoàn thành sương sương WishList(fakeProducts)

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,32 @@
-
 import React from 'react';
 
 import {createStackNavigator} from '@react-navigation/stack';
 import ProductListScreen from '../pages/WishList/ProductListScreen';
 import FavoritesScreen from '../pages/WishList/FavoritesScreen';
+import {NativeBaseProvider} from 'native-base';
 
 const Stack = createStackNavigator();
-
+// Định nghĩa kiểu RootStackParamList
+export type RootStackParamList = {
+    ProductList: undefined;
+    Favorites: undefined;
+};
 const App = () => {
     return (
-       <NativeBaseProvider>
-        <Stack.Navigator initialRouteName="ProductList">
-            <Stack.Screen
-                name="ProductList"
-                component={ProductListScreen}
-                options={{title: 'Danh sách sản phẩm'}}
-            />
-            <Stack.Screen
-                name="Favorites"
-                component={FavoritesScreen}
-                options={{title: 'Danh sách yêu thích'}}
-            />
-        </Stack.Navigator>
-         </NativeBaseProvider>
+        <NativeBaseProvider>
+            <Stack.Navigator initialRouteName="ProductList">
+                <Stack.Screen
+                    name="ProductList"
+                    component={ProductListScreen}
+                    options={{title: 'Danh sách sản phẩm'}}
+                />
+                <Stack.Screen
+                    name="Favorites"
+                    component={FavoritesScreen}
+                    options={{title: 'Danh sách yêu thích'}}
+                />
+            </Stack.Navigator>
+        </NativeBaseProvider>
 
     );
 };
