f95439363eea4bad070cc079af2cd5a64056430f
Phan Ngọc Tài <ngoctaiphan.cv@gmail.com>
2024-10-17 00:02:55 +0700

    Điều chỉnh phần navigation

--- pages/Login/Login.tsx
+++ pages/Login/Login.tsx
@@ -1,6 +1,14 @@
-import { useNavigation } from "@/router/NavigationContext";
-import { MaterialIcons } from "@expo/vector-icons";
+import api from "@/api/ApiService";
+import { userApi } from "@/api/UserApiService";
+import { RootStackParamList } from "@/app";
+import color from "@/assets/color/color";
+import Loading from "@/components/Animation/Loading";
+import { userLogin } from "@/interface/user";
+import { AntDesign, MaterialIcons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
 import {
+  Image,
   Box,
   Button,
   Center,
@@ -14,20 +22,19 @@ import {
   VStack,
   Text,
   WarningOutlineIcon,
+  Modal,
+  View,
 } from "native-base";
 import React, { useState } from "react";
-import { Alert, Pressable } from "react-native";
+import { Alert, Pressable, StyleSheet } from "react-native";
 
 export default function Login() {
-  const { navigate, goBack } = useNavigation();
+  const [modalVisible, setModalVisible] = useState(false);
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [errors, setErrors] = React.useState({});
-  const handleNavigate = () => {
-    navigate("Register");
-    console.log("Bấm nút");
-  };
   const validate = () => {
     let isValid = true;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -35,7 +42,6 @@ export default function Login() {
 
     // Reset errors trước khi kiểm tra
     setErrors({});
-
     // Kiểm tra email
     if (!email) {
       setErrors((prevError: any) => ({
@@ -43,12 +49,14 @@ export default function Login() {
         email: "Hãy nhập email của bạn",
       }));
       isValid = false;
+      return;
     } else if (!emailRegex.test(email)) {
       setErrors((prevError: any) => ({
         ...prevError,
         email: "Email không hợp lệ",
       }));
       isValid = false;
+      return;
     }
 
     // Kiểm tra mật khẩu
@@ -58,74 +66,88 @@ export default function Login() {
         password: "Hãy nhập mật khẩu của bạn",
       }));
       isValid = false;
-    } else if (!passwordRegex.test(password)) {
-      setErrors((prevError: any) => ({
-        ...prevError,
-        password:
-          "Mật khẩu phải chứa ít nhất 8 ký tự, bao gồm chữ in hoa, chữ in thường và số",
-      }));
-      isValid = false;
+      return;
     }
+    // else if (!passwordRegex.test(password)) {
+    //   setErrors((prevError: any) => ({
+    //     ...prevError,
+    //     password:
+    //       "Mật khẩu phải chứa ít nhất 8 ký tự, bao gồm chữ in hoa, chữ in thường và số",
+    //   }));
+    //   isValid = false;
+    //   return;
+    // }
 
     return isValid;
   };
-  const handleLogin = async () => {
+  const handleLogin = () => {
     //Kiểm tra dữ liệu nhập vào có hợp lệ không
     if (validate()) {
-      try {
-        const response = await fetch("đăng nhập", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: email,
-            password: password,
-          }),
-        });
-
-        if (!response.ok) {
-          const errorData = await response.json();
-          throw new Error(errorData.message || "Vui lòng thử lại");
-        }
-
-        const data = await response.json();
-        Alert.alert("Chào mừng", "Đăng nhập thành công!");
-      } catch (error: any) {
-        Alert.alert("Thất bại", error.message || "Lỗi kết nối");
-      }
+      login();
     }
+  };
+  const login = async () => {
+    const loginData: userLogin = { email, password };
+    console.log(loginData);
+
+    try {
+      const response = await userApi.getUserToken(loginData);
+      console.log(response);
+      if (response.status === 200) {
+        const data = response.data.data;
+
+        await AsyncStorage.setItem("token", data.token);
+        console.log(data.token);
+        // await (<Loading />);
+        navigation.navigate("ProductList");
+      }
+    } catch (error: any) {
+      // Kiểm tra nếu lỗi là từ response
+      const errorMessage =
+        error.response?.data?.message || error.message || "Lỗi kết nối";
+      Alert.alert("Thất bại", errorMessage);
+      console.log(error);
+    }
+  };
+  const handleResetPassword = () => {
+    navigation.navigate("Verify");
+    // if (validate()) {
+    //   console.log("Gửi mail: " + email);
+    // }
   };
 
   return (
-    <Center mt="50%" w="100%">
-      <Box safeArea p="2" py="8" w="90%" maxW="290">
-        <Heading
-          size="lg"
-          fontWeight="600"
-          color="coolGray.800"
-          _dark={{
-            color: "warmGray.50",
-          }}
-        >
-          Chào mừng
-        </Heading>
-        <Heading
-          mt="1"
-          _dark={{
-            color: "warmGray.200",
-          }}
-          color="coolGray.600"
-          fontWeight="medium"
-          size="xs"
-        >
-          Hãy đăng nhập để tiếp tục
-        </Heading>
+    <Center mt="20%" w="100%">
+      <Box safeArea p="2" py="8" w="100%" maxW="290">
+        <Center mb="3">
+          <Heading
+            size="lg"
+            fontWeight="600"
+            color="coolGray.800"
+            _dark={{
+              color: "warmGray.50",
+            }}
+          >
+            Chào mừng
+          </Heading>
+          <Heading
+            mt="1"
+            _dark={{
+              color: "warmGray.200",
+            }}
+            color="coolGray.600"
+            fontWeight="medium"
+            size="xs"
+          >
+            Hãy đăng nhập để tiếp tục
+          </Heading>
+        </Center>
 
         <VStack space={3} mt="5">
           <FormControl isRequired isInvalid={"email" in errors}>
             <FormControl.Label>Email</FormControl.Label>
             <Input
+              variant="rounded"
               value={email}
               onChangeText={setEmail}
               w={{
@@ -151,6 +173,8 @@ export default function Login() {
           <FormControl isRequired isInvalid={"password" in errors}>
             <FormControl.Label>Mật khẩu</FormControl.Label>
             <Input
+              variant="rounded"
+              style={{ borderRadius: 20 }}
               InputLeftElement={
                 <Icon
                   as={<MaterialIcons name="password" />}
@@ -191,17 +215,30 @@ export default function Login() {
               _text={{
                 fontSize: "xs",
                 fontWeight: "500",
-                color: "indigo.500",
+                color: "amber.900",
               }}
               alignSelf="flex-end"
               mt="1"
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
             >
               Quên mật khẩu?
             </Link>
           </FormControl>
-          <Button mt="2" colorScheme="indigo" onPress={handleLogin}>
+          <Button
+            mt="2"
+            colorScheme="indigo"
+            onPress={() => handleLogin()}
+            style={{
+              borderRadius: 20,
+              padding: 12,
+              backgroundColor: "#704F38",
+            }}
+          >
             Đăng nhập
           </Button>
+
           <HStack mt="6" justifyContent="center">
             <Text
               fontSize="sm"
@@ -214,27 +251,74 @@ export default function Login() {
             </Text>
             <Link
               _text={{
-                color: "indigo.500",
+                color: "amber.900",
                 fontWeight: "medium",
                 fontSize: "sm",
               }}
-              onPress={() => handleNavigate()}
+              onPress={() => navigation.navigate("Register")}
             >
               Đăng kí ngay
             </Link>
           </HStack>
         </VStack>
       </Box>
-      <Link
+      <View></View>
+      {/* <Link
         _text={{
           color: "red.500",
           fontWeight: "medium",
           fontSize: "sm",
         }}
-        onPress={goBack}
+        onPress={() => navigation.goBack}
       >
         Quay về trang trước đó
-      </Link>
+      </Link> */}
+      <Modal
+        isOpen={modalVisible}
+        onClose={() => setModalVisible(false)}
+        avoidKeyboard
+        justifyContent="flex-end"
+        bottom="4"
+        size="lg"
+      >
+        <Modal.Content>
+          <Modal.CloseButton />
+          <Modal.Header>Quên mật khẩu?</Modal.Header>
+          <Modal.Body>
+            Nhập email để thay đổi mật khẩu của bạn
+            <FormControl mt="3">
+              <FormControl.Label>Email</FormControl.Label>
+              <Input
+                value={email}
+                onChangeText={setEmail}
+                w={{
+                  base: "100%",
+                  md: "25%",
+                }}
+                InputLeftElement={
+                  <Icon
+                    as={<MaterialIcons name="email" />}
+                    size={5}
+                    ml="2"
+                    color="muted.400"
+                  />
+                }
+                keyboardType="email-address"
+              />
+              {"email" in errors ? (
+                <FormControl.ErrorMessage>
+                  {errors.email}
+                </FormControl.ErrorMessage>
+              ) : null}
+            </FormControl>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button flex="1" onPress={() => handleResetPassword()}>
+              Lấy lại mật khẩu
+            </Button>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
     </Center>
   );
 }
