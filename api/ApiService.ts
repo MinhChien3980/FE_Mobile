c1211bc61ddb84bf2b4cb820c1670703ae3d2a1f
HuynhThiMaiPhuong <21130149@st.hcmuaf.edu.vn>
2024-11-06 00:57:18 +0700

    contact

--- api/ApiService.ts
+++ api/ApiService.ts
@@ -1,85 +1,85 @@
-import axios, { AxiosResponse } from "axios";
+import axios, {AxiosResponse} from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 //Class này sẽ chứa các api mẫu dùng chung
 //Như là crud kèm theo token, riêng cái login thì chỉ cần gửi data là email và pass
 export default class ApiSerVice {
-  private readonly baseUrl: string = "http://192.168.1.131:8080/";
+    private readonly baseUrl: string = "http://10.0.2.2:8088";
 
-  // Hàm chung để gọi GET API
-  protected async get(endpoint: string): Promise<AxiosResponse<any>> {
-    const token = await this.getToken();
-    return axios({
-      url: `${this.baseUrl}${endpoint}`,
-      method: "GET",
-      headers: {
-        Authorization: token ? `Bearer ${token}` : undefined, // Nếu không có token, để undefined
-      },
-    });
-  }
+    // Hàm chung để gọi GET API
+    protected async get(endpoint: string): Promise<AxiosResponse<any>> {
+        const token = await this.getToken();
+        return axios({
+            url: `${this.baseUrl}${endpoint}`,
+            method: "GET",
+            headers: {
+                Authorization: token ? `Bearer ${token}` : undefined, // Nếu không có token, để undefined
+            },
+        });
+    }
 
-  // Hàm chung để gọi POST API
-  protected async post(
-    endpoint: string,
-    data: any
-  ): Promise<AxiosResponse<any>> {
-    const token = await this.getToken();
-    return axios({
-      url: `${this.baseUrl}${endpoint}`,
-      method: "POST",
-      data,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: token ? `Bearer ${token}` : undefined, // Nếu không có token, để undefined
-      },
-    });
-  }
+    // Hàm chung để gọi POST API
+    protected async post(
+        endpoint: string,
+        data: any
+    ): Promise<AxiosResponse<any>> {
+        const token = await this.getToken();
+        return axios({
+            url: `${this.baseUrl}${endpoint}`,
+            method: "POST",
+            data,
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token ? `Bearer ${token}` : undefined, // Nếu không có token, để undefined
+            },
+        });
+    }
 
-  // Hàm gọi login, không cần token
-  protected async login(
-    endpoint: string,
-    data: any
-  ): Promise<AxiosResponse<any>> {
-    return axios({
-      url: `${this.baseUrl}${endpoint}`,
-      method: "POST",
-      data,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  }
+    // Hàm gọi login, không cần token
+    protected async login(
+        endpoint: string,
+        data: any
+    ): Promise<AxiosResponse<any>> {
+        return axios({
+            url: `${this.baseUrl}${endpoint}`,
+            method: "POST",
+            data,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+    }
 
-  // Hàm chung để gọi PUT API
-  protected async put(
-    endpoint: string,
-    data: any
-  ): Promise<AxiosResponse<any>> {
-    const token = await this.getToken();
-    return axios({
-      url: `${this.baseUrl}${endpoint}`,
-      method: "PUT",
-      data,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: token ? `Bearer ${token}` : undefined, // Nếu không có token, để undefined
-      },
-    });
-  }
+    // Hàm chung để gọi PUT API
+    protected async put(
+        endpoint: string,
+        data: any
+    ): Promise<AxiosResponse<any>> {
+        const token = await this.getToken();
+        return axios({
+            url: `${this.baseUrl}${endpoint}`,
+            method: "PUT",
+            data,
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token ? `Bearer ${token}` : undefined, // Nếu không có token, để undefined
+            },
+        });
+    }
 
-  // Hàm chung để gọi DELETE API
-  protected async delete(endpoint: string): Promise<AxiosResponse<any>> {
-    const token = await this.getToken();
-    return axios({
-      url: `${this.baseUrl}${endpoint}`,
-      method: "DELETE",
-      headers: {
-        Authorization: token ? `Bearer ${token}` : undefined, // Nếu không có token, để undefined
-      },
-    });
-  }
+    // Hàm chung để gọi DELETE API
+    protected async delete(endpoint: string): Promise<AxiosResponse<any>> {
+        const token = await this.getToken();
+        return axios({
+            url: `${this.baseUrl}${endpoint}`,
+            method: "DELETE",
+            headers: {
+                Authorization: token ? `Bearer ${token}` : undefined, // Nếu không có token, để undefined
+            },
+        });
+    }
 
-  // Lấy token từ AsyncStorage
-  private async getToken(): Promise<string | null> {
-    return await AsyncStorage.getItem("token");
-  }
+    // Lấy token từ AsyncStorage
+    private async getToken(): Promise<string | null> {
+        return await AsyncStorage.getItem("token");
+    }
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,60 +1,66 @@
-
-
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import {createStackNavigator} from "@react-navigation/stack";
 import ProductListScreen from "../pages/WishList/ProductListScreen";
 import FavoritesScreen from "../pages/WishList/FavoritesScreen";
-import { NativeBaseProvider } from "native-base";
+import {NativeBaseProvider} from "native-base";
 import Home from "@/pages/Home/Home";
 import Login from "@/pages/Login/Login";
 import Register from "@/pages/Register/Register";
 import Verify from "@/pages/Verify";
-
+import {SafeAreaView} from "react-native";
+import EmailForm from "@/pages/Contact/EmailForm";
+import Contact from "@/pages/Contact/Contact";
 
 const Stack = createStackNavigator();
 
 // Định nghĩa kiểu dữ liệu cho navigation
 export type RootStackParamList = {
-  ProductList: undefined;
-  Favorites: undefined;
-  Register: undefined;
-  Login: undefined;
-  Verify: undefined;
+    ProductList: undefined;
+    Favorites: undefined;
+    Register: undefined;
+    Login: undefined;
+    Verify: undefined;
 };
 
 
 const App = () => {
-  return (
-    <NativeBaseProvider>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="ProductList"
-          component={ProductListScreen}
-          options={{ title: "Danh sách sản phẩm" }}
-        />
-        <Stack.Screen
-          name="Favorites"
-          component={FavoritesScreen}
-          options={{ title: "Danh sách yêu thích" }}
-        />
-        <Stack.Screen
-          name="Verify"
-          component={Verify}
-          options={{ title: "Xác nhận mã" }}
-        />
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ title: "Đăng nhập" }}
-        />
-        <Stack.Screen
-          name="Register"
-          component={Register}
-          options={{ title: "Đăng kí" }}
-        />
-      </Stack.Navigator>
-    </NativeBaseProvider>
-  );
+    return (
+        <NativeBaseProvider>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen
+                    name="Verify"
+                    component={Verify}
+                    options={{title: "Xác nhận mã"}}
+                />
+                <Stack.Screen
+                    name="Login"
+                    component={Login}
+                    options={{title: "Đăng nhập"}}
+                />
+                <Stack.Screen
+                    name="Register"
+                    component={Register}
+                    options={{title: "Đăng kí"}}
+                />
+                <Stack.Screen
+                    name="ProductList"
+                    component={ProductListScreen}
+                    options={{title: "Danh sách sản phẩm"}}
+                />
+                <Stack.Screen
+                    name="Favorites"
+                    component={FavoritesScreen}
+                    options={{title: "Danh sách yêu thích"}}
+                />
+                <Stack.Screen
+                    name="Contact"
+                    component={Contact}
+                    options={{title: "Liên hệ"}}
+                />
+            </Stack.Navigator>
+        </NativeBaseProvider>
+
+    );
 };
 
 export default App;
